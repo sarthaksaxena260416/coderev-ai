565e843be01de5d272c2a94f587a455b8bfe7456
Sarthak <sarthaksaxena2604@gmail.com>
2026-05-26 10:52:48 +0530

    fix cors for production

--- server/server.js
+++ server/server.js
@@ -11,7 +11,10 @@ const app = express();
 connectDB();
 
 app.use(cors({
-  origin: process.env.CLIENT_URL || "http://localhost:5173",
+  origin: [
+    "http://localhost:5173",
+    process.env.CLIENT_URL,
+  ],
   credentials: true,
 }));
 app.use(express.json({ limit: "10mb" }));
